6ef402ed5bc153cf3d9b64f7f86e5bb24fd02fc6
linogdtech-lab <linogdtech@gmail.com>
2026-09-04 17:14:35 +0000

    TESTE

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione a linha abaixo com o nome exato do seu repositório entre barras
+  base: "/GDTECH/", 
   plugins: [react()],
-  base: '/GDTECH/', // <--- Adicione esta linha exatamente assim
-})
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+});
