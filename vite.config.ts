51ccf040c87a2672ae0e9ffd7f40a1075a7c4fd6
linogdtech-lab <linogdtech@gmail.com>
2026-09-04 17:23:20 +0000

    a

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/GDTECH/", // <--- Adicione esta linha aqui
   server: {
     host: "::",
     port: 8080,
